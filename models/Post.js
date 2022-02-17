d0b8bebe031f3978c6dbf7f8b9023ab17d2e228e
Cameron Nelson <nelsonnoremac@gmail.com>
2022-02-17 17:12:25 -0600

    post functionality

--- models/Post.js
+++ models/Post.js
@@ -1,41 +1,41 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection')
+const sequelize = require('../config/connection');
 
 class Post extends Model {}
 
+// create fields/columns for Post model
 Post.init(
     {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        post_url: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                isUrl: true
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            refrences: {
-                model: 'user',
-                key: 'id'
-            }
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      post_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isURL: true
         }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
     },
     {
-        sequelize,
-        feezeTableName: true,
-        underscorded: true,
-        modelName: 'post'
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
     }
-);
-
-module.exports = Post
+  );
+  module.exports = Post;
